a0c77811f930a557b206851fcf1db53d93aef5e8
laras1234567 <00001099716342sp@al.educacao.sp.gov.br>
2024-12-04 08:39:31 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Filme',
+    'Quais são as três relíqueas de Harry Potter criado pela própria morte?',
+    ' Capa da Invisibilidade, Pedra da Ressurreição e a Varinha das Varinhas.'
+)
+
+criaCartao(
+    'Maquiagem',
+    'Para inicar a maquiagem, qual o priemiro passo?',
+    ' O certo seria inicar hidratando a pele'
+)
+
+criaCartao(
+    'Filme',
+    'Qual era o maior sonho de Rapunzel?',
+    'Seu maior sonho fora conseguir finalmente sair da torre na qual era presa!'
+)
+
+criaCartao(
+    'filme',
+    'Como arma de treino Merida de Valentes usava?',
+    'Merida ultilizava como instrumento um arco e flecha'
+)
